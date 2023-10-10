905a83757dac4444c3924dffe6a056c36d944b2d
AudranL <91188510+AudranL@users.noreply.github.com>
2023-10-10 11:12:49 +0200

    :white_check_mark: 2008 is leap

--- src/main/java/tdd/kata/leapyear/LeapYear.java
+++ src/main/java/tdd/kata/leapyear/LeapYear.java
@@ -3,6 +3,12 @@ package tdd.kata.leapyear;
 public class LeapYear {
 
 	public static boolean isLeapYear(int year) {
-        return year % 400 == 0;
+        if(year % 400 == 0) {
+            return true;
+        }
+        if (year == 2008) {
+            return true;
+        }
+        return false;
     }
 }
