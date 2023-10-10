abea50e983173b1d442d0af08a090c029099839c
AudranL <91188510+AudranL@users.noreply.github.com>
2023-10-10 11:27:07 +0200

    :recycle: refactor code

--- src/main/java/tdd/kata/leapyear/LeapYear.java
+++ src/main/java/tdd/kata/leapyear/LeapYear.java
@@ -3,15 +3,27 @@ package tdd.kata.leapyear;
 public class LeapYear {
 
 	public static boolean isLeapYear(int year) {
-        if (year % 100 == 0 && year % 400 != 0) {
+        if (isDivisibleBy100ButNotBy400(year)) {
             return false;
         }
-        if(year % 400 == 0) {
+        if(isDivisibleBy400(year)) {
             return true;
         }
-        if (year % 4 == 0) {
+        if (isDivisibleBy4(year)) {
             return true;
         }
         return false;
+    }
+
+    private static boolean isDivisibleBy4(int year) {
+        return year % 4 == 0;
+    }
+
+    private static boolean isDivisibleBy100ButNotBy400(int year) {
+        return year % 100 == 0 && !isDivisibleBy400(year);
+    }
+
+    private static boolean isDivisibleBy400(int year) {
+        return year % 400 == 0;
     }
 }
