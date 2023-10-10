475e08fdf69b26869f453de464ba732cc592ec5b
AudranL <91188510+AudranL@users.noreply.github.com>
2023-10-10 10:53:37 +0200

    :white_check_mark: 2017 is not leap

--- src/main/java/tdd/kata/leapyear/LeapYear.java
+++ src/main/java/tdd/kata/leapyear/LeapYear.java
@@ -3,6 +3,6 @@ package tdd.kata.leapyear;
 public class LeapYear {
 
 	public static boolean isLeapYear(int year) {
-		throw new IllegalStateException("Not implemented yet");
+		return false;
 	}
 }
