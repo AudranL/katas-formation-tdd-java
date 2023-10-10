36b51f15a2cc6f5fdf7ab3b549f86e2d3e57d369
AudranL <91188510+AudranL@users.noreply.github.com>
2023-10-10 11:21:03 +0200

    :white_check_mark: 1700 is not leap

--- src/main/java/tdd/kata/leapyear/LeapYear.java
+++ src/main/java/tdd/kata/leapyear/LeapYear.java
@@ -3,6 +3,9 @@ package tdd.kata.leapyear;
 public class LeapYear {
 
 	public static boolean isLeapYear(int year) {
+        if (year == 1700) {
+            return false;
+        }
         if(year % 400 == 0) {
             return true;
         }
