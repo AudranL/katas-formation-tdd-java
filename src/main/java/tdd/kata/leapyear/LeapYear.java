ec312e02dc90e58f3292e04090233c61ea4a5bd5
AudranL <91188510+AudranL@users.noreply.github.com>
2023-10-10 10:57:54 +0200

    :white_check_mark: 1600 is leap

--- src/main/java/tdd/kata/leapyear/LeapYear.java
+++ src/main/java/tdd/kata/leapyear/LeapYear.java
@@ -3,6 +3,6 @@ package tdd.kata.leapyear;
 public class LeapYear {
 
 	public static boolean isLeapYear(int year) {
-        return year == 2000;
+        return year == 2000 || year == 1600;
     }
 }
