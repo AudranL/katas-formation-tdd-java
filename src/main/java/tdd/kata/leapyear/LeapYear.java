951cfd746fa3a4b83e46a128cbb03b705a485805
AudranL <91188510+AudranL@users.noreply.github.com>
2023-10-10 10:56:25 +0200

    :recycle: refactor code

--- src/main/java/tdd/kata/leapyear/LeapYear.java
+++ src/main/java/tdd/kata/leapyear/LeapYear.java
@@ -3,9 +3,6 @@ package tdd.kata.leapyear;
 public class LeapYear {
 
 	public static boolean isLeapYear(int year) {
-		if (year == 2000) {
-			return true;
-		}
-		return false;
-	}
+        return year == 2000;
+    }
 }
